2f73f1bac2b3e225a0d2a25b6482397a95088aaf
simstim-star <raidemall01@gmail.com>
2024-04-28 16:12:38 -0300

    Add HelloFrameBuffering

---
+++ Samples/Desktop/D3D12HelloWorld/src/HelloFrameBuffering/README.md
@@ -0,0 +1,22 @@
+## DirectX12 HelloFrameBuffering in C
+This is an adaptation of the official [D3D12HelloWorld HelloFrameBuffering](https://github.com/microsoft/DirectX-Graphics-Samples/tree/master/Samples/Desktop/D3D12HelloWorld/src/HelloFrameBuffering) using C.
+
+This was done for educational purposes, in order to learn better about COM and DirectX.
+
+## How to build (MSVC)
+Run the following with CMake:
+
+```
+cmake -B build-msvc -S . -G "Visual Studio 17 2022"
+cmake --build build-msvc
+```
+
+This will generate a folder called `build-msvc` with the Visual Studio Solution and the project files.
+
+## How to build (GCC)
+
+You can also build with GCC:
+
+```
+cc -mwindows *.c -ldxgi -ld3dcompiler -ld3d12 -ldxguid
+```
---
+++ Samples/Desktop/D3D12HelloWorld/src/HelloFrameBuffering/sample.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "Windows.h"
+#include <stdbool.h>
+#include <d3d12.h>
+
+#define FrameCount 2
+
+typedef struct IDXGISwapChain3 IDXGISwapChain3;
+
+typedef struct DXSample
+{
+    // Viewport dimensions.
+    UINT width;
+    UINT height;
+    float aspectRatio;
+    // Adapter info.
+    bool useWarpDevice;
+    // Root assets path.
+    WCHAR assetsPath[512];
+    // Window title.
+    CHAR *title;
+
+    // Pipeline objects.
+    D3D12_VIEWPORT viewport;
+    D3D12_RECT scissorRect;
+    IDXGISwapChain3 *swapChain;
+    ID3D12Device *device;
+    ID3D12Resource *renderTargets[FrameCount];
+    ID3D12CommandAllocator *commandAllocators[FrameCount];
+    ID3D12CommandQueue *commandQueue;
+    ID3D12RootSignature *rootSignature;
+    ID3D12DescriptorHeap *rtvHeap;
+    ID3D12PipelineState *pipelineState;
+    ID3D12GraphicsCommandList *commandList;
+
+    UINT rtvDescriptorSize;
+
+    // App resources.
+    ID3D12Resource *vertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+
+    // Synchronization objects.
+    UINT frameIndex;
+    HANDLE fenceEvent;
+    ID3D12Fence *fence;
+    UINT64 fenceValues[FrameCount];
+} DXSample;
+
+
+void Sample_Init(DXSample* const sample);
+void Sample_Destroy(DXSample* const sample);
+void Sample_Update(DXSample* const sample);
+void Sample_Render(DXSample* const sample);
