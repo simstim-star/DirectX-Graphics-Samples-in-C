0b54bd91b5dcf7359358cec7f8bc1a6a81b8dfb6
simstim-star <raidemall01@gmail.com>
2024-07-21 22:05:44 -0300

    simplify macro with anonymous struct inside union

--- Samples/Desktop/D3D12DepthBoundsTest/src/PipelineStateSubobjects.h
+++ Samples/Desktop/D3D12DepthBoundsTest/src/PipelineStateSubobjects.h
@@ -2,32 +2,27 @@
 #include <d3d12.h>
 
 /******************************************************************************************************************************************************
-The macro below defines two types and a function:
-- struct __PIPELINE_STATE_STREAM_[[PIPELINE_STATE_TYPE]]: token that will be packed in a stream to be consumed by CreatePipelineState. It is
-required that all data has the format of type + object.
+The macro below defines a types and a converter function:
 - a union CD3DX12_PIPELINE_STATE_STREAM_[[PIPELINE_STATE_TYPE]]: D3D12 runtime expects subobjects to be aligned to the natural word alignment 
 of the system. This union is a way to guarantee that instead of using alignas.
-- TO_PIPELINE_STATE_STREAM_[[PIPELINE_STATE_TYPE]]: a function to convert from type to the equivalent __PIPELINE_STATE_STREAM_[[PIPELINE_STATE_TYPE]]
+- TO_PIPELINE_STATE_STREAM_[[PIPELINE_STATE_TYPE]]: a function to convert from type to the equivalent CD3DX12_PIPELINE_STATE_STREAM_[[PIPELINE_STATE_TYPE]]
 
 Reference: https://learn.microsoft.com/en-us/windows/win32/api/d3d12/ns-d3d12-d3d12_pipeline_state_stream_desc
 ******************************************************************************************************************************************************/
-#define CD3DX12_PIPELINE_STATE_STREAM(PIPELINE_STATE_TYPE, TYPE) typedef struct __PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE \
-{                                                   \
-	D3D12_PIPELINE_STATE_SUBOBJECT_TYPE Type;       \
-	TYPE object;                                    \
-} __PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE;  \
-                                                    \
-typedef union CD3DX12_PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE  \
+#define CD3DX12_PIPELINE_STATE_STREAM(PIPELINE_STATE_TYPE, TYPE) typedef union CD3DX12_PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE  \
 {                                                                    \
-	__PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE concrete;        \
-	void* voidPtr;                                                   \
+	struct {                                                         \
+		D3D12_PIPELINE_STATE_SUBOBJECT_TYPE type;                    \
+		TYPE object;                                                 \
+	};                                                               \
+	void* raw;                                                       \
 } CD3DX12_PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE;             \
                                                                      \
 CD3DX12_PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE TO_PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE(TYPE obj) {  \
-   CD3DX12_PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE token;                       \
-   token.concrete.Type = D3D12_PIPELINE_STATE_SUBOBJECT_TYPE_## PIPELINE_STATE_TYPE;  \
-   token.concrete.object = obj;                                                       \
-   return token;                                                                      \
+   CD3DX12_PIPELINE_STATE_STREAM_ ## PIPELINE_STATE_TYPE token;              \
+   token.type = D3D12_PIPELINE_STATE_SUBOBJECT_TYPE_## PIPELINE_STATE_TYPE;  \
+   token.object = obj;                                                       \
+   return token;                                                             \
 }
 
 CD3DX12_PIPELINE_STATE_STREAM(ROOT_SIGNATURE, ID3D12RootSignature*)
